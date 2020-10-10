23e47b6b058fbe0fa1c3c54cc95c0b443a5a4b1a
Ashish Kashyap <ashishkashyapdude@gmail.com>
2020-10-10 13:50:21 +0530

    Products Task
    - Added Entity Data Model Context
    - Created new controller with new action that returns all products
    - Created new ProductDto model class

---
+++ ForeverDigitalTaskTest/ForeverDigitalTaskTest/Controllers/ProductsController.cs
@@ -0,0 +1,18 @@
+﻿using ForeverDigitalTaskTest.DB;
+using ForeverDigitalTaskTest.Models;
+using System.Linq;
+using System.Web.Http;
+
+namespace ForeverDigitalTaskTest.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        public IHttpActionResult Get()
+        {
+            using (ForeverDigitalEntities db = new ForeverDigitalEntities())
+            {
+                return Ok(db.Products.ToList().Select(x => new ProductDto(x)));
+            }
+        }
+    }
+}
